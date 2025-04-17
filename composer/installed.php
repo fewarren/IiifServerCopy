6e8bf8347201a473d3a4417cfc43b3d8d20c746c
fewarren <FWarren574@aol.com>
2025-04-17 12:29:40 -0700

    Add files via upload

---
+++ composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => 'daniel-km/iiif-server',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '94dd63e84b7a22093742155ce8a1823e7ee166de',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'daniel-km/iiif-server' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '94dd63e84b7a22093742155ce8a1823e7ee166de',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'doctrine/inflector' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '*',
+            ),
+        ),
+        'james-heinrich/getid3' => array(
+            'pretty_version' => 'v2.0.0-beta6',
+            'version' => '2.0.0.0-beta6',
+            'reference' => '8bf46222ae008d870e6676b4bf455ed664d90d05',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../james-heinrich/getid3',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
